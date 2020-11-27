e7caef7e68f0804eab1c8e6aa6f9cfe1a4e1fcd7
Luis Gustavo Araujo <luisaraujo.ifba@gmail.com>
2020-11-27 01:54:03 -0300

    update project structure

--- api.php
+++ api.php
@@ -1,13 +1,32 @@
 <?php
 
-$token = "AAA";//$_POST["idtoken"];
+function generateRandomString($length = 10) { 
+	$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'; 
+	
+	$charactersLength = strlen($characters); 
+
+	$randomString = '';
+
+	for ($i = 0; $i < $length; $i++) { 
+		$randomString .= $characters[rand(0, $charactersLength - 1)];
+	} 
+
+	return $randomString;
+}
+
+do{
+	$token = "files_exec/".generateRandomString();
+
+}while( is_dir($token) );
+
+
+//$_POST["idtoken"];
 $code = $_POST['code'];
+$lang = isset($_POST['lang'])?$_POST['lang']:"c";
+
 //Creating folder
 $uritoken= $token."/";
 
-unlink($uritoken."code.c");	
-unlink($uritoken."file.exe");	
-rmdir($token);
 
 
 if( !mkdir($uritoken) ){
@@ -15,25 +34,50 @@ if( !mkdir($uritoken) ){
 	return;
 }
 
+if($lang == "c"){
+	
+	$namefile =$uritoken."code.c";
+	$myfile = fopen($namefile, "w");
+	fwrite($myfile, $code);
+	fclose($myfile);
+	$cmd = "gcc ".$namefile." -o ".$uritoken."file.exe 2> ".$uritoken."output";
 
-$namefile =$uritoken."code.c";
-$myfile = fopen($namefile, "w");
+}else if($lang == "py"){
+	
+	$namefile =$uritoken."code.py";
+	$myfile = fopen($namefile, "w");
+	fwrite($myfile, $code);
+	fclose($myfile);
+	$cmd = "python ".$namefile;
+}
 
-fwrite($myfile, $code);
-
-$cmd = "gcc ".$namefile." -o ".$uritoken."file.exe 2> output";
 exec($cmd, $result, $error);
 
 if($error){
 	
 	echo '{"status": "error"}';
-	return;
+	//return;
 }else{
 	
 	echo '{"status": "noerror"}';
-	return;
+	//return;
 }
 
+sleep(1);
+
+if( file_exists($uritoken."output") )
+	unlink($uritoken."output");
+
+if( file_exists($uritoken."code.c") )
+	unlink($uritoken."code.c");
+
+if( file_exists($uritoken."code.py"))
+	unlink($uritoken."code.py");
+
+if( file_exists($uritoken."file.exe"))	
+	unlink($uritoken."file.exe");
+if(is_dir($token))
+	rmdir($token);
 
 
 ?>
